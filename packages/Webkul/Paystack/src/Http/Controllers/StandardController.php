f04ee222877e7458d2c67cc1334e1590555f3594
Mawuli Agbenyo <mawulikofiagbenyo@gmail.com>
2024-08-21 02:06:03 +0000

    `Refactor Paystack payment class to remove debug statement`

--- packages/Webkul/Paystack/src/Http/Controllers/StandardController.php
+++ packages/Webkul/Paystack/src/Http/Controllers/StandardController.php
@@ -26,7 +26,7 @@ class StandardController extends Controller
      */
     public function redirect()
     {
-        dd(request()->all());
+        ddd();
         return view('paystack::standard-redirect');
     }
 
